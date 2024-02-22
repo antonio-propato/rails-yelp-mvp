27e951c0e625c497754d19ac58ae4d09a92026f6
Antonio Propato <tonyprp@gmail.com>
2024-02-22 08:51:56 +0000

    Rails-Yelp

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,11 @@
+class ReviewsController < ApplicationController
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+end
